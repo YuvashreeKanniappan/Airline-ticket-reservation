857cfb678b02ac46424262e8c00bf6cbfd66f0c7
YuvashreeKanniappan <46278401+YuvashreeKanniappan@users.noreply.github.com>
2024-04-02 14:21:29 +0530

    Add files via upload

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,45 @@
+//  import { Component, OnInit, Output } from '@angular/core';
+//  import { Product } from 'src/app/models/product';
+//  import { ProductService } from 'src/app/services/product.service';
+//  import { RouterService } from 'src/app/services/router.service';
+//  import Swal from 'sweetalert2';
+//  import {HttpClientModule, HttpResponseBase} from '@angular/common/http';
+// import {HttpClient} from '@angular/common/http';
+// import { HttpParams } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+
+//  @Component({
+//   selector: 'app-search',
+//    templateUrl: './search.component.html',
+//    styleUrls: ['./search.component.css']
+// })
+//  export class SearchComponent implements OnInit  {
+//     //observableticket: Observable<Product[]>;
+//     tickets: Product[] =[];
+
+ 
+//    constructor(private productservice: ProductService, private http: HttpClientModule, private param: HttpParams, private httpclint : HttpClient,
+//     private productmodel : Product, private routerService: RouterService) { 
+//         //this.observableticket = this.productservice.getUsersMultipleParams();
+//     }
+
+    
+
+   
+
+//   ngOnInit(): void {
+
+//     // getSearchresults()
+//   // {
+// //     this.observableticket = this.productservice.getUsersMultipleParamss();
+// //     this.observableticket.subscribe(
+// //     pass =>{ this.tickets = pass;
+// //     console.log('pass', pass);
+// //     })
+// //    this.routerService.gotoSearchFlights();
+// // }
+
+//   }
+
+//  }
